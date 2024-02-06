924e9b69c5f38ef4673b7ab5f346566a96251657
theocarter <111892118+theocarter@users.noreply.github.com>
2024-02-06 16:19:40 -0500

    Add files via upload

---
+++ SeniorSemReadMe.py
@@ -0,0 +1,28 @@
+newlist = []
+
+inputNum = input('Enter 3 or more numbers seperated by space \n')
+
+newlist = inputNum.split()
+
+for i in range(len(newlist)):
+    newlist[i] = int(newlist[i])
+
+def insertionsort(num):
+
+    for i in range(1,len(num)):
+        j = num[i]
+
+        k = i-1
+
+        while k >= 0 and j < num[k]:
+            num[k+1]= num[k]
+            k -= 1
+            num[k+1]= j
+
+
+print(newlist)
+
+
+insertionsort(newlist)
+print(newlist)
+
